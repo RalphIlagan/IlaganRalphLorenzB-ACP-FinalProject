3ca05e27afdcd2fecbc8070afd831dbc2667109b
RalphIlagan <23-06919@g.batstate-u.edu.ph>
2024-11-28 21:09:52 +0800

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import tkinter as tk
-from communitysupportapp import CommunitySupportApp # Correct import statement
+from communitysupportapp import CommunitySupportApp
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = CommunitySupportApp(root)
     root.mainloop()
+
